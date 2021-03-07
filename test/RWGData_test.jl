0e7e2e7b512e644cd8f0e889743043799f60a3bf
Peter Simon <psimon0420@gmail.com>
2021-03-06 20:36:20 -0800

    Change rwgbfft to rwgbfft!

--- test/RWGData_test.jl
+++ test/RWGData_test.jl
@@ -1,5 +1,6 @@
 using PSSFSS
 using PSSFSS.RWG
+using StaticArrays: MArray
 using Test
 
 
@@ -35,19 +36,23 @@ end
     @test rwgdata.nufp == 16
 end
 
-@testset "rwgbfft" begin
+@testset "rwgbfft!" begin
     sheet = rectstrip(Lx=1, Ly=1, Px=1, Py=1, Nx=2, Ny=1, units=mm, fufp=false)
     rwgdat = setup_rwg(sheet)
+    nbf = size(rwgdat.bff,2)
+    ft = zeros(MArray{Tuple{2},ComplexF64,1,2}, (nbf))
     k = [0.,0.]; ψ₁ = 0.0; ψ₂ = 0.0
-    ft = rwgbfft(rwgdat, sheet, k, ψ₁, ψ₂)
+    rwgbfft!(ft, rwgdat, sheet, k, ψ₁, ψ₂)
     @test ft[1] ≈ [0.0001666666666666667 + 0.0im, -0.0003333333333333334 + 0.0im]
     @test ft[2] ≈ [0.0003333333333333334 + 0.0im, 0.0003333333333333334 + 0.0im]
     @test ft[3] ≈ [0.00016666666666666663 + 0.0im, -0.0003333333333333334 + 0.0im]
 
     sheet = rectstrip(Lx=1, Ly=1, Px=1, Py=1, Nx=2, Ny=1, units=inch, fufp=false)
     rwgdat = setup_rwg(sheet)
+    nbf = size(rwgdat.bff,2)
+    ft = zeros(MArray{Tuple{2},ComplexF64,1,2}, (nbf))
     k = [20.0,-30.3]; ψ₁ = 0.8; ψ₂ = -0.7
-    ft = rwgbfft(rwgdat, sheet, k, ψ₁, ψ₂)
+    rwgbfft!(ft, rwgdat, sheet, k, ψ₁, ψ₂)
     @test ft[1] ≈ [0.003986416685586114 - 0.0010511300702857503im, -0.008079847138694855 + 0.0021304773084817956im]
     @test ft[2] ≈ [0.008263557134402535 - 0.0010871639073400372im, 0.008284527320353664 - 0.0010899227712197804im]
     @test ft[3] ≈ [0.004122638196063312 - 1.5707327530113332e-5im, -0.008355946971820058 + 3.1836312009155855e-5im]
