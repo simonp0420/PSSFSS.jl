70c1fd8ee37324db7c1efcf3875067fbc068da89
Documenter.jl <documenter@juliadocs.github.io>
2023-03-22 02:13:26 +0000

    build based on 4b84c1b

--- dev/PSS_&_FSS_Element_Gallery/manji/demo_manji2.jl
+++ dev/PSS_&_FSS_Element_Gallery/manji/demo_manji2.jl
@@ -1,11 +1,10 @@
 using PSSFSS, Plots
-L1 = 0.481
-L2 = 0.22
+L1, L2, L3, L4 = 0.481, 0.22, 0.24, 1.1
 w = 0.06
-L3 = 0.24
 a = 0.15
-w2 = 0.0
-sheet = manji(; s1=[1.1, 0], s2=[0, 1.1], units=cm, L1, L2, L3, w, w2, a, CCW=true, ntri=1000)
+w2 = 0.03
+s1=[1.2, 0]; s2=[0, 1.2]
+sheet = manji(; s1, s2, units=cm, L1, L2, L3, L4, w, w2, a, CCW=true, ntri=1000)
 p1 = plot(sheet, linecolor=:red, size=(400,400), unitcell=true)
 p2 = plot(sheet, linecolor=:blue, rep=(3,3))
 plot(p1, p2, layout = (1,2), size=(800,400), margin=10Plots.pt)
