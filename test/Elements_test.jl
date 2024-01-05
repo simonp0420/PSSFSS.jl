07c0ddbeabbe121a052d5c6dc49c3c3fe99a2ef2
Peter Simon <psimon0420@gmail.com>
2024-01-04 20:04:13 -0800

    Revert test for Apple system

--- test/Elements_test.jl
+++ test/Elements_test.jl
@@ -118,6 +118,6 @@ end
     sides = 50; ntri = 2800; units = cm
     sheet = sinuous(; arms=4, b, w=0.03, rc=0.05, s1, s2,
                    L2=0.95, w2=0.03, c2=0.12, g=0.04, sides, ntri, units)
-    mycount = Sys.isapple() ? 3328 : 3393
-    @test facecount(sheet) == mycount
+    # mycount = Sys.isapple() ? 3328 : 3393
+    @test facecount(sheet) == 3393
 end
