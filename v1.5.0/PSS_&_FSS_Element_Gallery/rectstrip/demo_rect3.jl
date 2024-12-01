034e6124bf0061364f29012d0012d9cb31923214
Peter Simon <simonp0420@users.noreply.github.com>
2024-12-01 11:08:47 -0800

    Delete v1.5.0 directory

--- v1.5.0/PSS_&_FSS_Element_Gallery/rectstrip/demo_rect3.jl
+++
@@ -1,8 +0,0 @@
-using Plots, PSSFSS
-strip = rectstrip(Nx=8, Ny=4, Px=1, Py=0.2, Lx=0.15, Ly=0.2, units=cm)
-p1 = plot(strip, unitcell=true, linecolor=:red)
-p2 = plot(strip, rep=(3,5), linecolor=:blue)
-plot(p1, p2, layout=(1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
-
