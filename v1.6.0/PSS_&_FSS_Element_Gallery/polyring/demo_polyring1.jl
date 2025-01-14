fe60b0f107ec72b586bf8e4bb4bb57452400cb30
Peter Simon <simonp0420@users.noreply.github.com>
2025-01-14 05:45:00 -0800

    Delete v1.6.0/PSS_&_FSS_Element_Gallery/polyring directory

--- v1.6.0/PSS_&_FSS_Element_Gallery/polyring/demo_polyring1.jl
+++
@@ -1,8 +0,0 @@
-using PSSFSS, Plots
-sheet = polyring(s1=[1,0], s2=[0,1], a=[0.2, 0.45], b=[0.35, 0.55], sides=4, orient = 45, units=cm, ntri=800)
-p1 = plot(sheet, linecolor=:red, unitcell=true)
-p2 = plot(sheet, linecolor=:blue, rep=(4,3))
-plot(p1, p2, layout = (1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
-
