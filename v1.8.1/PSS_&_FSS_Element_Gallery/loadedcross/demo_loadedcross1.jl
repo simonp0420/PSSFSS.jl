fb3eff7632812a67032668d5f02ed2aa76c49b54
Peter Simon <simonp0420@users.noreply.github.com>
2025-01-14 05:57:36 -0800

    Delete v1.8.1 directory

--- v1.8.1/PSS_&_FSS_Element_Gallery/loadedcross/demo_loadedcross1.jl
+++
@@ -1,7 +0,0 @@
-using PSSFSS, Plots
-sheet = loadedcross(s1=[1,0], s2=[0,1], L1=0.9, L2=0.2, w=0.35, units=cm, ntri=400)
-p1 = plot(sheet, linecolor=:red, unitcell=true)
-p2 = plot(sheet, linecolor=:blue, rep=(4,3))
-plot(p1, p2, layout = (1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
