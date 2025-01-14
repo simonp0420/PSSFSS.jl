055fe54ba87e3bdd61ab3c1c4902ae62d384f14b
Peter Simon <simonp0420@users.noreply.github.com>
2025-01-14 05:51:46 -0800

    Delete v1.7.1/PSS_&_FSS_Element_Gallery/diagstrip directory

--- v1.7.1/PSS_&_FSS_Element_Gallery/diagstrip/demo_diagstrip1.jl
+++
@@ -1,7 +0,0 @@
-using PSSFSS, Plots
-sheet = diagstrip(P=5.2, w=0.21, units=mm, Nl=60, Nw=4, orient=45)
-p1 = plot(sheet, linecolor=:red, unitcell=true)
-p2 = plot(sheet, linecolor=:blue, rep=(4,3))
-plot(p1, p2, layout = (1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
