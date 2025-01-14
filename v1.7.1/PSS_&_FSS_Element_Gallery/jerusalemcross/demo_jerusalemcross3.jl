e06563ed415b32ea328f0dd539990a8a1c937b09
Peter Simon <simonp0420@users.noreply.github.com>
2025-01-14 05:52:00 -0800

    Delete v1.7.1/PSS_&_FSS_Element_Gallery/jerusalemcross directory

--- v1.7.1/PSS_&_FSS_Element_Gallery/jerusalemcross/demo_jerusalemcross3.jl
+++
@@ -1,7 +0,0 @@
-using PSSFSS, Plots
-sheet = jerusalemcross(P=1, L1=0.9, L2=0.12, w=0.04, A = 0.4, B = 0.08, units=cm, ntri=800)
-p1 = plot(sheet, linecolor=:red, unitcell=true)
-p2 = plot(sheet, linecolor=:blue, rep=(4,3))
-plot(p1, p2, layout = (1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
