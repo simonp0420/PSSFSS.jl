1785bb3b64824991497b5f24fc30e6228923c915
Peter Simon <simonp0420@users.noreply.github.com>
2025-01-14 05:53:08 -0800

    Delete v1.7.1/PSS_&_FSS_Element_Gallery directory

--- v1.7.1/PSS_&_FSS_Element_Gallery/meander/demo_meander1.jl
+++
@@ -1,7 +0,0 @@
-using PSSFSS, Plots
-sheet = meander(a=0.3535, b=0.707, h=0.28, w1=0.018, w2=0.018, ntri=600, units=inch,rot=45)
-p1 = plot(sheet, linecolor=:green, unitcell=true)
-p2 = plot(sheet, linecolor=:green, rep=(4,3))
-plot(p1, p2, layout = (1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
--- v1.7.1/PSS_&_FSS_Element_Gallery/polyring/demo_polyring4.jl
+++
@@ -1,7 +0,0 @@
-using PSSFSS, Plots
-sheet = polyring(s1=[1, 0], s2=[0.5, √3/2], a=[0.0, 0.45], b=[0.15, 0.55], sides=6, units=cm, orient=30, ntri=600)
-p1 = plot(sheet, linecolor=:red, unitcell=true)
-p2 = plot(sheet, linecolor=:blue, rep=(4,3))
-plot(p1, p2, layout = (1,2), size=(800,400))
-
-# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
