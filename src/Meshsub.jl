06f3a6e76894d0b98e6061b3a5fb04f1a327a6e8
Peter Simon <psimon0420@gmail.com>
2025-03-06 11:37:14 -0800

    Move triin setup inside iteration loop to hopefully avoid occational segfault

--- src/Meshsub.jl
+++ src/Meshsub.jl
@@ -62,15 +62,16 @@ function meshsub(; points::Matrix{<:Real}, seglist::Matrix{<:Integer},
     end
 
     # Set up for call to triangulate:
-    triin = TriangulateIO()
-    triin.pointlist = Matrix{Cdouble}(points)
-    triin.segmentlist = Matrix{Cint}(seglist)
-    isempty(segmarkers) || (triin.segmentmarkerlist = Vector{Cint}(segmarkers))
-    isempty(holes) || (triin.holelist = Matrix{Cdouble}(holes))
-
+    triout = TriangulateIO()  # Establish scope
     areaold = area
-    triout = deepcopy(triin) # Establish scope
     for k in 1:iter
+        # Set up for call to triangulate:
+        triin = TriangulateIO()
+        triin.pointlist = Matrix{Cdouble}(points)
+        triin.segmentlist = Matrix{Cint}(seglist)
+        isempty(segmarkers) || (triin.segmentmarkerlist = Vector{Cint}(segmarkers))
+        isempty(holes) || (triin.holelist = Matrix{Cdouble}(holes))
+
         # Perform triangulation:
         (triout, vorout) = triangulate(switches, triin)
         ntrinew = size(triout.trianglelist, 2)
