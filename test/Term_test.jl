db2bab854916f910ca308c24ef2df7a0600625fc
Peter Simon <psimon0420@gmail.com>
2026-07-11 19:51:23 -0700

    Correct term unit test

--- test/Term_test.jl
+++ test/Term_test.jl
@@ -7,6 +7,6 @@ using Test
 @testset "term" begin
     tests = (PSSFSS._is_ijulia(), PSSFSS._is_vsc_integrated_term(), PSSFSS._is_vsc_notebook(),
              PSSFSS._is_pluto_notebook())
-    @test count(tests) == 1
+    @test count(tests) < 2
     @test PSSFSS.run_environment() isa String
 end
