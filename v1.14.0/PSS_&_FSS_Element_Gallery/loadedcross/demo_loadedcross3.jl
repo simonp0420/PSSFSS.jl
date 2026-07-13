94882517fc0b39cfee99d317b767c5f657646e0a
Documenter.jl <documenter@juliadocs.github.io>
2026-07-13 02:28:19 +0000

    build based on ef26072

---
+++ v1.14.0/PSS_&_FSS_Element_Gallery/loadedcross/demo_loadedcross3.jl
@@ -0,0 +1,7 @@
+using PSSFSS, Plots
+sheet = loadedcross(s1=[1,0], s2=[0.5,sqrt(3)/2], L1=0.9, L2=0.2, w=0.35, units=cm, ntri=300, orient=45)
+p1 = plot(sheet, linecolor=:red, unitcell=true)
+p2 = plot(sheet, linecolor=:blue, rep=(4,3))
+plot(p1, p2, layout = (1,2), size=(800,400))
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
